65bbba672a67837dcb81a8f218cae5b78c611ee4
Unknown <jessyrivard@icloud.com>
2019-10-17 10:39:44 -0400

    API request to find specific task added

--- src/api.js
+++ src/api.js
@@ -2,10 +2,17 @@ var encodedURI = window.encodeURI(process.env.REACT_APP_BACKEND_URL + "api/");
 
 module.exports = {
   fetchLists: function() {
-    return window.axios(encodedURI + "lists/", { method: "GET" })
-    .then(response => {
-      console.log(response);
-      return response.data;
-    });
+    return window
+      .axios(encodedURI + "lists/", { method: "GET" })
+      .then(response => {
+        return response.data;
+      });
+  },
+  fetchTask: function(selected) {
+    return window
+      .axios(encodedURI + "tasks/find/" + selected, { method: "GET" })
+      .then(response => {
+        return response.data;
+      });
   }
 };
